a9d49ceed9a2653b71d4bc994269072631f1541d
Autumn-Martin <autumnmartin.dev@gmail.com>
2018-07-25 13:33:43 -0600

    WIP

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -1,3 +1,20 @@
 class Pantry
+  attr_reader :stock
+  def initialize
+    @stock = {}
+  end
 
+  def stock_check(item)
+    # @stock[:item]
+    if @stock.include?(item)
+      @stock[:item]
+    else
+      0
+    end
+  end
+
+  def restock(item, amount)
+    @stock[:item] = 'amount' #=> 10
+    # require 'pry'; binding.pry
+  end
 end
--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,5 +3,25 @@ require 'minitest/autorun'
 require 'minitest/pride'
 
 class PantryTest < Minitest::Test
+  def test_it_exists
+    pantry = Pantry.new
+    assert_instance_of Pantry, pantry
+  end
+  # # Checking and adding stock
+  def test_stock_can_be_added
+    pantry = Pantry.new
+    assert_equal ({}), pantry.stock
+  end
 
+  def test_it_can_be_restocked
+    pantry = Pantry.new
+    assert_equal 0, pantry.stock_check("Cheese")
+    pantry.restock("Cheese", 10)
+    assert_equal 10, pantry.stock_check("Cheese")
+  end
+  # # => 10
+  #
+  # pantry.restock("Cheese", 20)
+  # pantry.stock_check("Cheese")
+  # # => 30
 end
